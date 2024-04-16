30619c3b2919aa15853a4d582f682080b8ce2370
alire1992 <alirezasa24@gmail.com>
2024-04-16 18:09:36 +0330

    Set starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+
+// Selecting element
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const dice = document.querySelector(".dice");
+
+// Starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
